9aa06d48d3a4f6cf74ca2f77525a77428f743bc2
Jussi <jussi.pesonen@adaptek.fi>
2024-03-11 02:28:32 +0200

    First draft of using Serial.write() for PCM transfer.

--- glitch-sturm/def.hpp
+++ glitch-sturm/def.hpp
@@ -1,12 +1,17 @@
 #define ledPin 13
 #define speakerPin 11
+
 #define rightButtonPin 2
 #define leftButtonPin 4
+
 #define ledBit0Pin 7
 #define ledBit1Pin 8
 #define ledBit2Pin 9
 #define ledBit3Pin 10
-#define isDebugging false
+
 #define leftPotPin 0
 #define midPotPin 1
 #define rightPotPin 2
+
+#define isDebugging false
+#define streamSerial true
---
+++ glitch-sturm/demos/README.md
@@ -0,0 +1,4 @@
+# Demos
+
+The serial streaming of the signal as PCM data worked to some extent. This could open up all sorts of stuff. `serialtune.mp3`
+is the result of my first experimentation.
