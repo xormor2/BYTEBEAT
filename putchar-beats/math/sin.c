8c272a1d064dc55414874ec75805846a792fe1ba
Jussi <jussi.pesonen@adaptek.fi>
2024-03-11 06:56:08 +0200

    Added new math generator function sin.

--- putchar-beats/math/sin.c
+++ putchar-beats/math/sin.c
@@ -3,43 +3,39 @@ CC By Sa jeesus-bock & xormor2 2024
 gcc sin.c -lm -o sin
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <unistd.h>
+#include <time.h>
 
 float mogrify(float t)
 {
-    double tmp = sin(t);
-    return fabs(tmp) * 20;
+    return sin(t / 20) * 20 + 20;
 }
 int main(float t)
 {
     float mod = 1;
-    float modDelta = .01;
-    for (;; t += .001)
+    float modDelta = .02;
+    for (;; t += (rand() % 100) / 100000.0)
     {
 
         mod += modDelta;
         if (mod > 1.5)
         {
-            modDelta = -.002;
+            modDelta = -.02;
         }
         if (mod < 0.5)
         {
-            modDelta = .002;
+            modDelta = .02;
         }
         float val = mogrify(t);
-        if (val * mod > 50)
-        {
-            val /= mod;
-        }
-        else
-        {
-            val *= mod;
-        };
+        val *= mod;
 
-        putchar((char)val);
-        usleep(10);
-        // printf("%d \n", (char)val);
+        // printf("%d\n", (char)val);
+        putchar(val);
+        struct timespec ts;
+        ts.tv_sec = 0;
+        ts.tv_nsec = rand() % 999;
+        nanosleep(&ts, NULL);
     }
     return 0;
 }
