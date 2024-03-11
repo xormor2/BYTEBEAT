2360203229ea310af7f624edd591c33d360b77c0
Lauri "Linux" Friberg <30544305+xormor2@users.noreply.github.com>
2024-03-11 09:33:30 +0000

    Add files via upload

--- putchar-beats/playtune.c
+++ putchar-beats/playtune.c
@@ -9,10 +9,26 @@ int main(int argc, char *argv[])
   }
   else if (argc>1)
   {
-      if(!strcmp("playtune",argv[1]))
+      if(!strcmp("playtune1",argv[1]))
       {
         for(;;t++)putchar(((t<<1)^((t<<3)+(t>>10)&t>>12))|t>>(4-(1^7&(t>>18)))|t>>7);
       }
+		else if(!strcmp("playtune2",argv[1]))
+		{
+			for(;;t++)putchar(((t<<2)^((t<<1)+(t>>7)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7);
+		}
+		else if(!strcmp("playtune3",argv[1]))
+		{
+			for(;;t++)putchar(((t<<3)^((t<<1)+(t>>7)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7);
+		}
+		else if(!strcmp("playtune4",argv[1]))
+		{
+			for(;;t++)putchar(((t<<3)^((t<<4)+(t>>5)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7);
+		}
+		if(!strcmp("playtune5",argv[1]))
+		{
+			for(;;t++)putchar(((t<<3)^((t<<4)+(t>>5)&t>>12))|t>>(4-(1^7&(t>>23)))|t>>7);
+		}
       else
       {
         puts("bad commandline argument!");
